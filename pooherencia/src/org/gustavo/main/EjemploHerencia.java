a72a497b2073922edd683dbe55419a3ffe4fdafa
gustavo177 <gtvadf177@hotmail.com>
2024-07-13 16:36:06 -0500

    Aplicando Módulos

---
+++ pooherencia/src/org/gustavo/main/EjemploHerencia.java
@@ -0,0 +1,23 @@
+package org.gustavo.main;
+
+import org.gustavo.pooherencia.Alumno;
+import org.gustavo.pooherencia.Profesor;
+
+public class EjemploHerencia {
+    public static void main(String[] args) {
+        Alumno alumno = new Alumno();
+        alumno.setNombre("Andres");
+        alumno.setApellido("Gusman");
+
+        Profesor profesor = new Profesor();
+        profesor.setNombre("Luci");
+        profesor.setApellido("Pérez");
+        profesor.setAsignatura("Matemáticas");
+
+        System.out.println(alumno.getNombre() + " " + alumno.getApellido());
+        System.out.println("Profesor " + profesor.getAsignatura()
+                + ": " + profesor.getNombre()
+                + " " + alumno.getApellido());
+
+    }
+}
