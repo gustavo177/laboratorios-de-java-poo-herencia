7663e2a1f5d9d8a4d499d0bd07e865cac2e743ee
gustavo177 <gtvadf177@hotmail.com>
2024-07-14 11:59:26 -0500

    aplicando cast

--- src/org/gustavo/ejemplo/EjemploHerencia.java
+++ src/org/gustavo/ejemplo/EjemploHerencia.java
@@ -1,17 +1,25 @@
-package org.gustavo.pooherencia;
+package org.gustavo.ejemplo;
+
+import org.gustavo.pooherencia.Alumno;
+import org.gustavo.pooherencia.Persona;
+import org.gustavo.pooherencia.Profesor;
 
 public class EjemploHerencia {
     public static void main(String[] args) {
-        Alumno alumno = new Alumno();
+        Persona alumno = new Alumno();
         alumno.setNombre("Andres");
         alumno.setApellido("Gusman");
+
+        ((Alumno)alumno).setInstitucion("Instituto Nacional");
 
         Profesor profesor = new Profesor();
         profesor.setNombre("Luci");
         profesor.setApellido("Pérez");
         profesor.setAsignatura("Matemáticas");
 
-        System.out.println(alumno.getNombre() + " " + alumno.getApellido());
+        System.out.println(alumno.getNombre()
+                + " " + alumno.getApellido()
+                + " " + ((Alumno) alumno).getInstitucion());
         System.out.println("Profesor " + profesor.getAsignatura()
                 + ": " + profesor.getNombre()
                 + " " + profesor.getApellido());
