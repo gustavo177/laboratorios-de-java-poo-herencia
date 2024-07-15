f40dfe04dee4fb73763a1dd2dd92247b34c75b48
gustavo177 <gtvadf177@hotmail.com>
2024-07-15 12:45:41 -0500

    Herencia y el API de reflexión de Java

--- src/org/gustavo/ejemplo/EjemploHerencia.java
+++ src/org/gustavo/ejemplo/EjemploHerencia.java
@@ -7,8 +7,21 @@ public class EjemploHerencia {
         Alumno alumno = new Alumno();
         alumno.setNombre("Andres");
         alumno.setApellido("Gusman");
-
         alumno.setInstitucion("Instituto Nacional");
+        alumno.setNotaCastellano(5.5);
+        alumno.setNotaHistoria(6.3);
+        alumno.setNotaMatematica(4.9);
+
+        AlumnoInternacional alumnoInt=new AlumnoInternacional();
+        alumnoInt.setNombre("Peter");
+        alumnoInt.setApellido("Gosling");
+        alumnoInt.setPais("Australia");
+        alumnoInt.setEdad(15);
+        alumnoInt.setInstitucion("Instituto Nacional");
+        alumnoInt.setNotaIdiomas(6.8);
+        alumnoInt.setNotaCastellano(6.2);
+        alumnoInt.setNotaHistoria(5.8);
+        alumnoInt.setNotaMatematica(4.5);
 
         Profesor profesor = new Profesor();
         profesor.setNombre("Luci");
@@ -18,8 +31,23 @@ public class EjemploHerencia {
         System.out.println(alumno.getNombre()
                 + " " + alumno.getApellido()
                 + " " + alumno.getInstitucion());
+
+        System.out.println(alumnoInt.getNombre()
+                + " " + alumnoInt.getApellido()
+                + " " + alumnoInt.getInstitucion()
+                + " " + alumnoInt.getPais());
+
         System.out.println("Profesor " + profesor.getAsignatura()
                 + ": " + profesor.getNombre()
                 + " " + profesor.getApellido());
+
+        Class clase = alumnoInt.getClass();
+        while (clase.getSuperclass() != null){
+            String hija = clase.getName();
+            String padre = clase.getSuperclass().getName();
+            System.out.println(hija + " es una clase hija de la clase padre " + padre);
+            clase = clase.getSuperclass();
+        }
+
     }
 }
