f3923f4aaed908c62d76a746923979e04769e0e0
gustavo177 <gtvadf177@hotmail.com>
2024-07-30 19:50:14 -0500

    Imagen UML

---
+++ actividadAlmacenDeVentaPorMayor/src/com/gustavo/ejemplo/Main.java
@@ -0,0 +1,37 @@
+package com.gustavo.ejemplo;
+
+/*
+* Un almacén de venta por mayor vende productos alimenticios perecederos
+* y no perecederos y artículos de limpieza. Tiene dos vendedores. Desarrollar
+*  un diagrama de clases que modele esta situación teniendo en cuenta a sus clientes.
+
+Ademas escriba con código java la jerarquía de clases que están presentes.
+* */
+
+/* Respuesta
+* Jerarquía 1
+* public class Producto {}
+
+    public class Alimento extends Producto {}
+
+    public class Perecedero extends Alimento {}
+
+    public class NoPerecedero extends Alimento {}
+
+    public class Limpieza extends Producto {}
+
+* Jerarquía 2
+*
+    public class Persona {}
+
+    public class Vendedor extends Persona {}
+
+    public class Cliente extends Persona {}
+* */
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
